9b09b9387c7c82b260b72f5861a71bb72254b083
unknown <alexander.schilling@stud.altekanti.ch>
2018-11-14 10:45:52 +0100

    commit

---
+++ WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die
+' Assemblyinformationen zu ändern.
+
+' Werte der Assemblyattribute überprüfen
+<Assembly: AssemblyTitle("WebApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM bereitgestellt wird.
+<Assembly: Guid("9b41849d-ef7d-4961-97f4-3bfe8836fbb2")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" wie folgt verwenden:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ WebApplication1/js/myjs.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+  
+    $(".karte").mouseover(function () {
+        $(this).css("cursor", "pointer");
+    });
+
+    $(".karte").mouseenter(function () {
+            $(this).addClass("enter");
+        });
+
+    $(".karte").mouseleave(function () {
+            $(this).removeClass("enter");
+        });  
+});
+
+$(document).ready(function () {
+    $("#footer").css("font-weight", "bold");
+    $("#mainheader").css("padding", "20px");
+    $(".maintext").css("color", "#0769AA ");
+});
+
+$(document).ready(function () {
+    $("#mainheader").click(function () {
+        $(this).css("border-style", "solid");
+        $(this).css("border-color", "#0769AA");
+        $(".maintext").css("font-size", "25px");
+        $("#footer").text("Doppelklick verkleinert wieder...")
+    });
+});
+
+$(document).ready(function () {
+    $("#mainheader").dblclick(function () {
+        location.reload();
+    });
+});
+
